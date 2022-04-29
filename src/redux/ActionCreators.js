88e79f07cfb3a0747fcd8797dfa2f8c6ceb6ef51
Anshav Upadhyay Nepal <anshabupadhyay@gmail.com>
2022-04-29 14:03:34 -0700

    fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -16,22 +16,70 @@ export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
 
     return fetch(baseUrl + 'campsites')
-    .then(response => response.json())
-    .then(campsites => dispatch(addCampsites(campsites)));
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            const error = new Error(`Error ${response.status}: ${response.statusText}`);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        const errMess = new Error(error.message);
+        throw errMess;
+    }
+)
+.then(response => response.json())
+.then(campsites => dispatch(addCampsites(campsites)))
+.catch(error => dispatch(campsitesFailed(error.message)));
 };
+
+
 
 export const fetchComments = () => dispatch => {    
     return fetch(baseUrl + 'comments')
-        .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)));
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            const error = new Error(`Error ${response.status}: ${response.statusText}`);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        const errMess = new Error(error.message);
+        throw errMess;
+    }
+)
+.then(response => response.json())
+.then(comments => dispatch(addComments(comments)))
+.catch(error => dispatch(commentsFailed(error.message)));
 };
+
 
 export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
 
     return fetch(baseUrl + 'promotions')
-        .then(response => response.json())
-        .then(promotions => dispatch(addPromotions(promotions)));
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            const error = new Error(`Error ${response.status}: ${response.statusText}`);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        const errMess = new Error(error.message);
+        throw errMess;
+    }
+)
+.then(response => response.json())
+.then(promotions => dispatch(addPromotions(promotions)))
+.catch(error => dispatch(promotionsFailed(error.message)));
 };
 
 
